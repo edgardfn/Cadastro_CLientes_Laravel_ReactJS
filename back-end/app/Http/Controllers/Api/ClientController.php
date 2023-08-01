b5090e980f8ee34124cb7ec6d44a747c424d7cd1
Edgard FInotti Neto <edgardfinotti@yahoo.com.br>
2023-08-01 14:48:18 -0300

    criado rota de listar clientes, consultar cliente, adicionar novo cliente e atualizar cliente. Criado nova migration para atualizar o campo data de nascimento para null.

---
+++ back-end/app/Http/Controllers/Api/ClientController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Clients;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    /**
+     * @var Client
+     */
+    private $client;
+
+    public function __construct(Clients $client)
+    {
+        $this->client = $client;
+    }
+
+    public function index()
+    {
+        $clients = $this->client->all();
+
+        return response()->json($clients);
+    }
+
+    public function show($id)
+    {
+        $client = $this->client->find($id);
+
+        return response()->json($client);
+    }
+
+    public function save(Request $request)
+    {
+        $data = $request->all();
+        $client = $this->client->create($data);
+
+        return response()->json($client);
+    }
+
+    public function update(Request $request)
+    {
+        $data = $request->all();
+
+        $client = $this->client->find($data['id']);
+        $client->update($data);
+
+        return response()->json($client);
+    }
+}
--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +15,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['apiJwt']], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::namespace('App\\Http\\Controllers\\Api')->prefix('clients')->group(function () {
+
+        //Rota de listar clientes:
+        Route::get('/', 'ClientController@index');
+        //Rota de consultar cliente especifico:
+        Route::get('/{id}', 'ClientController@show');
+        //Rota de salvar novo cliente:
+        Route::post('/', 'ClientController@save');
+        //Atualizar cliente:
+        Route::put('/', 'ClientController@update');
+    });
+
+    //Rota de listar clientes:
+    //Route::get('/clients', 'App\\Http\\Controllers\\Api\\ClientController@index');
+
+    //Rota de salvar novo cliente:
+    //Route::post('/clients', 'App\\Http\\Controllers\\Api\\ClientController@save');
 });
 
+// Route::middleware('api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+//Rota de clientes:
+// Route::middleware('api')->get('/clients', function () {
+//     return \App\Models\Clients::all();
+// });
+
+//Rota de Login:
+Route::post('/auth/login', 'App\\Http\\Controllers\\Api\\AuthController@login');
+
+/*
 Route::post('/login', function (Request $request) {
     $credentials = $request->only(['email', 'password']);
 
@@ -33,3 +67,4 @@ Route::post('/login', function (Request $request) {
         ],
     ]);
 });
+*/
