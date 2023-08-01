9d9819b3040390acff2f454c5d9b621d14627974
Edgard FInotti Neto <edgardfinotti@yahoo.com.br>
2023-08-01 15:43:30 -0300

    inserido biblioteca styled components, preparado aplicação para receber temas padrões e estilos globais e configurado a tipagem dos temas padrões.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './themes/default'
+
 export function App() {
-  return <>Teste</>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <div>teste</div>
+    </ThemeProvider>
+  )
 }
